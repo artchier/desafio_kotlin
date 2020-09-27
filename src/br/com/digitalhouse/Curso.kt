69c7136eb2832ca160bd304d40ed3cdc75b24116
artchier <ar-chaves@hotmail.com>
2020-09-27 20:59:50 -0300

    Parte H -  Criando os métodos adicionarUmAluno e excluirAluno

--- src/br/com/digitalhouse/Curso.kt
+++ src/br/com/digitalhouse/Curso.kt
@@ -1,6 +1,6 @@
 package br.com.digitalhouse
 
-class Curso(var nome: String, var codigoCurso: Int) {
+class Curso(var nome: String, var codigoCurso: Int, var vagasDisponiveis: Int, var alunos: MutableMap<Int, Aluno>) {
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -10,5 +10,13 @@ class Curso(var nome: String, var codigoCurso: Int) {
         if (codigoCurso == other.codigoCurso) return true
 
         return true
+    }
+
+    fun adicionarUmAluno(umAluno: Aluno): Boolean{
+        return (vagasDisponiveis > 0)
+    }
+
+    fun excluirAluno(umAluno: Aluno){
+        alunos.remove(umAluno.codigoAluno, umAluno)
     }
 }
