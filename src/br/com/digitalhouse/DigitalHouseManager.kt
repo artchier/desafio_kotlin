40aaf8aa2909183ffb5a4437b8fcc0ff6a5d1175
artchier <ar-chaves@hotmail.com>
2020-09-28 17:38:27 -0300

    Parte I - Criando novos métodos em DigitalHouseManager

--- src/br/com/digitalhouse/DigitalHouseManager.kt
+++ src/br/com/digitalhouse/DigitalHouseManager.kt
@@ -1,7 +1,102 @@
 package br.com.digitalhouse
 
-data class DigitalHouseManager (var alunos: MutableMap<Int, Aluno>,
-                                var professores: MutableMap<Int, Professor>,
-                                var cursos: MutableMap<Int, Curso>,
-                                var matriculas: MutableMap<Curso, List<Aluno>>) {
+data class DigitalHouseManager(var alunos: MutableList<Aluno>,
+                               var professores: MutableList<Professor>,
+                               var cursos: MutableList<Curso>,
+                               var matriculas: MutableList<Matricula>) {
+
+    fun registrarCurso(nome: String, codigoCurso: Int, qtdMaximaDeAlunos: Int) {
+        cursos.add(Curso(nome, codigoCurso, qtdMaximaDeAlunos))
+    }
+
+    fun excluirCurso(codigoCurso: Int) {
+        var achou: Boolean = false
+        cursos.forEach {
+            if(it.codigoCurso == codigoCurso){
+                cursos.remove(it)
+                achou = true
+                println("Curso excluído")
+            }
+        }
+        if(!achou)
+            println("Curso não encontrado")
+    }
+
+    fun registrarProfessorAdjunto(nome: String, sobrenome: String, codigoProfessor: Int, horasMonitoria: Int){
+        professores.add(ProfessorAdjunto(nome, sobrenome, 0, codigoProfessor, horasMonitoria))
+    }
+
+    fun registrarProfessorTitular(nome: String, sobrenome: String, codigoProfessor: Int, especialidade: String){
+        professores.add(ProfessorTitular(nome, sobrenome, 0, codigoProfessor, especialidade))
+    }
+
+    fun excluirProfessor(codigoProfessor: Int){
+        var achou: Boolean = false
+        professores.forEach {
+            if (it.codigoProfessor == codigoProfessor){
+                professores.remove(it)
+                achou = true
+            }
+        }
+        if(!achou)
+            println("Professor não encontrado")
+    }
+
+    fun registrarAluno(nome: String, sobrenome: String, codigoAluno: Int){
+        alunos.add(Aluno(nome, sobrenome, codigoAluno))
+    }
+
+    fun matricularAluno(codigoAluno: Int, codigoCurso: Int){
+        val achou = arrayOf(false, false)
+        for(num in 0 .. cursos.size){
+            if(cursos[num].codigoCurso == codigoCurso){
+                alunos.forEach {
+                    if(it.codigoAluno == codigoAluno){
+                        if(cursos[num].adicionarUmAluno(it))
+                            matriculas.add(Matricula(it, cursos[num]))
+                        else
+                            println("Curso sem vagas disponíveis")
+                        achou[1] = true
+                    }
+                }
+                achou[0] = true
+            }
+        }
+        if(!achou[0])
+            println("Curso não encontrado")
+        else if(!achou[1])
+            println("Aluno não encontrado")
+    }
+
+    fun alocarProfessores(codigoCurso: Int, codigoProfessorTitular: Int, codigoProfessorAdjunto: Int){
+        val achou = arrayOf(false, false, false)
+        for(num in 1 .. cursos.size){
+            when(professores[num].codigoProfessor){
+                (codigoProfessorTitular) -> {
+                    cursos.forEach {
+                        if (it.codigoCurso == codigoCurso) {
+                            it.alocaProfessores(professores[num])
+                            println("Novo professor titular alocado")
+                            achou[2] = true
+                        }
+                    }
+                    achou[0] = true
+                }
+                (codigoProfessorAdjunto) -> {
+                    cursos.forEach {
+                        if(it.codigoCurso == codigoCurso){
+                            it.alocaProfessores(professores[num])
+                            println("Novo professor adjunto alocado")
+                            achou[2] = true
+                        }
+                    }
+                    achou[1] = true
+                }
+            }
+        }
+        if(!achou[2])
+            println("Professor não encontrado")
+        else if(!achou[0])
+            println("Curso não encontrado")
+    }
 }
--- src/br/com/digitalhouse/ProfessorAdjunto.kt
+++ src/br/com/digitalhouse/ProfessorAdjunto.kt
@@ -1,8 +1,23 @@
 package br.com.digitalhouse
 
-class ProfessorAdjunto(var horasMonitoria: Int,
-                       nome: String,
+class ProfessorAdjunto(nome: String,
                        sobrenome: String,
                        tempoDeCasa: Int,
-                       codigoProfessor: Int) : Professor(nome, sobrenome, tempoDeCasa, codigoProfessor) {
+                       codigoProfessor: Int,
+                       var horasMonitoria: Int) : Professor(nome, sobrenome, tempoDeCasa, codigoProfessor) {
+
+    init{
+        println("Novo Professor Adjunto Registrado\nNome: $nome $sobrenome" +
+                "\nTempo de Casa: $tempoDeCasa\nCódigo: $codigoProfessor\nQuantidade de Horas de Monitoria: $horasMonitoria")
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        if (other !is Professor) return false
+
+        if (codigoProfessor == other.codigoProfessor) return true
+
+        return true
+    }
 }
