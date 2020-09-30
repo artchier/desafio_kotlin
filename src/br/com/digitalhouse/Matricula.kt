920638f242e80748f4766963682f9cbe56524e34
artchier <ar-chaves@hotmail.com>
2020-09-30 19:03:59 -0300

    Penúltimo Commit

--- src/br/com/digitalhouse/Matricula.kt
+++ src/br/com/digitalhouse/Matricula.kt
@@ -1,11 +1,13 @@
 package br.com.digitalhouse
 
+import java.text.SimpleDateFormat
 import java.util.*
 
-class Matricula (aluno: Aluno, curso: Curso){
+class Matricula (aluno: Aluno, curso: Curso) {
 
     var data = Date()
-    init{
+
+    init {
         println("Nova Matrícula Registrada\nCurso: ${curso.nome}\nAluno matriculado: ${aluno.nome} ${aluno.sobrenome}" +
                 "\nCódigo do aluno: ${aluno.codigoAluno}\nData de matrícula: $data\n\n")
     }
